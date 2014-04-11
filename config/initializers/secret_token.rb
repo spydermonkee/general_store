dc34a15549b6b3ab888ab3813c99ca8ff8096121
Spyder Monkee McHenry & Elikem Adadevoh <student@epicodus.com>
2014-04-11 11:16:38 -0700

    adds devise, cancan to an initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Store::Application.config.secret_key_base = 'b53dc55b66cb589e9c18294542b88aa6b7d077698ee4b9b391770aa447a8a4d175d94faa99af20ec01e52cb8c40315146c1a606dc2406bf1fd23865b74ccdd3b'
